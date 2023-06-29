738ce79231b740f6920e356655c4fef8023ea27b
rushikeesh <rushikesh.wayal.ai@ghrcem.raisoni.net>
2023-06-29 18:57:10 +0530

    new things added trying to push

--- src/hackerrank/primitives.java
+++ src/hackerrank/primitives.java
@@ -4,37 +4,78 @@ import java.util.Scanner;
 import java .math.MathContext;
 public class primitives {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int t = input.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//
+//        for (int i 0 1; i < t; i++) {
+//
+//            try {
+//                long x = sc.nextLong();
+//                System.out.println(x + " can be fitted in:");
+//
+//
+//                if (x >= -128 && x <= 127) {
+//                    System.out.println("*byte");
+//                    System.out.println("*short");
+//                    System.out.println("*int");
+//                    System.out.println("*long");
+//                }
+//                else if (x >= -32768 && x <= 32767) {
+//                    System.out.println("*short");
+//                    System.out.println("*int");
+//                    System.out.println("*long");
+//                }
+//                else if (x >= -2147483648 && x <= 2147483647) {
+//                    System.out.println("*int");
+//                    System.out.println("*long");
+//                }
+//                else if (x >= -9223372036854775808L && x <= 9223372036854775807L) {
+//                    System.out.println("*long");
+//                }
+//            } catch (Exception e) {
+//                System.out.println(input.next()+" can't be fitted anywhere.");
+//
+//            }
+//        }
 
-        for (int i = 1; i <= t; i++) {
+                Scanner sc = new Scanner(System.in);
+                int t=sc.nextInt();
 
-            try {
-                long x = input.nextLong();
-                System.out.println(x + " can be fitted in:");
+                for(int i=0;i<t;i++)
+                {
 
+                    try
+                    {
+                        long x=sc.nextLong();
 
-                if (x >= -128 && x <= 127) {
-                    System.out.println("*byte");
-                    System.out.println("*short");
-                    System.out.println("*int");
-                    System.out.println("*long");
+                        System.out.println(x+" can be fitted in:");
+                        if(x>=-128 && x<=127){
+                            System.out.println("* byte");
+                            System.out.println("* short");
+                            System.out.println("* int");
+                            System.out.println("* long");
+                        }
+                        else if (x>=-32768 && x<=32767)
+                        {
+                            System.out.println("* short");
+                            System.out.println("* int");
+                            System.out.println("* long");
+                        }
+                        else if (x>=-2147483648 && x<=2147483647)
+                        {
+                            System.out.println("* int");
+                            System.out.println("* long");
+                        }
+                        else if (x>=-9223372036854775808L && x<=9223372036854775807L){
+                            System.out.println("* long");
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        System.out.println(sc.next()+" can't be fitted anywhere.");
+                    }
+
                 }
-                else if (x >= -32768 && x <= 32767) {
-                    System.out.println("*short");
-                    System.out.println("*int");
-                    System.out.println("*long");
-                }
-                else if (x >= -2147483648 && x <= 2147483647) {
-                    System.out.println("*int");
-                    System.out.println("*long");
-                }
-                else if (x >= -9223372036854775808L && x <= 9223372036854775807L) {
-                    System.out.println("*long");
-                }
-            } catch (Exception e) {
-                System.out.println(input.next()+" can't be fitted anywhere.");
             }
         }
-    }
-}
+
