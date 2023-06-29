86e3c3c972d2b542f2252b9e97de1a622579ac19
rushikeesh <rushikesh.wayal.ai@ghrcem.raisoni.net>
2023-06-29 13:05:10 +0530

    new things added trying to push

---
+++ src/com/Front/java/until/hackerrank.java
@@ -0,0 +1,16 @@
+package com.Front.java.until;
+
+import java.util.Scanner;
+
+public class hackerrank {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int N = input.nextInt();
+        for (int i=1;i<=10;i++){
+            int result= N *i;
+            System.out.printf("%d x %d = %d%n",N,i,result);
+
+
+        }
+    }
+}
