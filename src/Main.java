c781ee9cc435bcc634eba51f33427672bf635e20
rushikeesh <rushikesh.wayal.ai@ghrcem.raisoni.net>
2023-06-30 20:58:12 +0530

    new things added trying to push

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
-        System.out.println("DJ SIR");
-        System.out.println("3353535");
-        System.out.print("hello world   ");
-        System.out.println(3445);
-        System.out.println(3+3);
-        System.out.println(4*8);
-        System.out.println(4/3);
-        float a=4;
-        float b=3;
-        float z=a/b;
-        System.out.println(z);
+//        System.out.println("Hello world!");
+//        System.out.println("DJ SIR");
+//        System.out.println("3353535");
+//        System.out.print("hello world   ");
+//        System.out.println(3445);
+//        System.out.println(3+3);
+//        System.out.println(4*8);
+//        System.out.println(4/3);
+//        float a=4;
+//        float b=3;
+//        float z=a/b;
+//        System.out.println(z);
 //        Scanner x = new Scanner(System.in);
 //        Scanner y = new Scanner(System.in);
 //        System.out.println(x.nextInt());
 //        System.out.println(y.nextInt());
+
+//        int a= 33_33_343,b=22_00_000;
+//        System.out.println(" a = "+a+" b = "+b);
+//
+//        int k=666;
+//        byte g=(byte)k;
+//        System.out.println(g);
+//        System.out.println(666%256   );
+        int i= 50000;
+        float f=33.3f;
+        double d=32.4324554;
+        short s=1220;
+        char c='a';
+        byte b=123;
+        double result=(f*b)+(i/c)-(d*s);
+        System.out.println((f*b)+" + "+(i/c)+" - "+(d*s));
+        System.out.println(result);
+
+
+
 
 
     }
